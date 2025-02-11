040f1f7b0f8d78f2c479bbd47905399425d496cc
AlexYami <alexeykamaev@yandex.ru>
2025-02-11 20:21:58 +0400

    add webchat markup in handlebars

---
+++ src/components/imageButton/index.ts
@@ -0,0 +1,3 @@
+import "./imageButton.scss";
+
+export { default as ImageButton } from "./imageButton.hbs?raw";
---
+++ src/pages/signin/index.ts
@@ -0,0 +1,2 @@
+// import './login.css'
+export { default as SigninPage } from "./signin.hbs?raw";
